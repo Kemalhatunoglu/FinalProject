bcea788fbf2b3439cccf4ebbd37479bcee850e93
Kemal Hatunoglu <64790356+Kemalhatunoglu@users.noreply.github.com>
2021-01-31 20:29:21 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Abstract klasötlerin içerisine referans tutucular konulacak yani soyut classlarımızı koyacağız.
+            // Concrete klasörlerine somut classlarımızı modellerimizi koyacağız.
+
+            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+            foreach (var item in productManager.GetAll())
+            {
+                Console.WriteLine(item.ProductName);
+            }
+        }
+    }
+}
