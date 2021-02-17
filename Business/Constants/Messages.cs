9db5410e3c202c3cdf343ff6c72ef2cceccf61ef
Kemal Hatunoğlu <kemalhatunoglu@gmail.com>
2021-02-18 00:12:26 +0300

    17 Şubat Ders sonu AOP FluentValidation'lar eklendi zor tekrar et

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,6 @@ namespace Business.Constants
         public static string MaintenanceTime = "Sistem bakımda.";
         public static string ProductsListed = "Ürünler listelendi.";
         public static string CategoryIdListed = "Ürünler kategoriye göre listelendi.";
+        internal static string UnitPriceInvalid = "Ürün uyumsuz.";
     }
 }
